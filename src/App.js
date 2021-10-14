61722ab44a7186a690dcc21f0592ebc25530e79f
flimby2010 <87784870+flimby2010@users.noreply.github.com>
2021-10-14 17:58:09 -0300

    Add Dictionary API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Dictionary />
         Hello!!! Soon this will be a dictionary App
       </header>
     </div>
