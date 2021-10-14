b7d232c4f00f7a879cb0eed3745218bf418c1411
flimby2010 <87784870+flimby2010@users.noreply.github.com>
2021-10-14 16:55:02 -0300

    Inittial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello!!! Soon this will be a dictionary App
+      </header>
+    </div>
+  );
+}
+
+export default App;
